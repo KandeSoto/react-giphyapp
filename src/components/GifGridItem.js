fbc8ae257a806f683f0472a66f8829fc68c9f9fd
KandeSoto <kandelariosoto@gmail.com>
2022-12-14 13:57:24 -0800

    muchos gifs

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function GifGridItem({ title, url }) {
+  return (
+    <div className="card animate__animated animate__fadeIn">
+      <img src={url} alt={title} />
+      <p>{title}</p>
+    </div>
+  );
+}
